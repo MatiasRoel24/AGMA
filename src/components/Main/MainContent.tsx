03d6aa0ffa0d009d5b825b60cf30e4204dd88e88
Matias Ezequiel Roel <roel@fktech.net>
2024-04-05 12:02:29 -0300

    Base

---
+++ src/components/Main/MainContent.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Hero } from '../hero/Hero'
+import animate from '../../media/animate.json'
+import Lottie from 'lottie-react'
+import { ButtonMoving } from '../button/ButtonMoving';
+
+export const MainContent = () => {
+    return (
+        <div className='bg-neutral-900 flex flex-col lg:flex-row items-center h-[120vh] md:h-screen w-full justify-center content-center'>
+            <div className='h-40 flex flex-col gap-16 justify-center content-start items-center'>
+                <Hero />
+                <ButtonMoving />
+            </div>
+            <div >
+                <Lottie animationData={animate} loop={true} />
+            </div>
+        </div>
+    )
+}
+
---
+++ src/components/button/ButtonMoving.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { Button } from "../ui/moving-border";
+
+export function ButtonMoving() {
+
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    // Simula un retraso de 2 segundos antes de mostrar el botón
+    const timer = setTimeout(() => {
+      setShowButton(true);
+    }, 100);
+
+    // Limpia el temporizador cuando el componente se desmonta
+    return () => clearTimeout(timer);
+  }, []); // El efecto se ejecuta solo una vez al montar el componente
+
+  return (
+    <div>
+      {showButton && (<Button
+        borderRadius="1.75rem"
+        className="
+        
+        dark:bg-neutral-200 
+        text-white 
+        dark:text-black 
+        border-cyan-400 
+        dark:border-neutral-200 
+        border-2
+        "
+      >
+        Agendar reunion
+      </Button>
+      )}
+    </div>
+  );
+}
